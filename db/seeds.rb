1bbafd0ca85b17dd95421ed0c236c9b9d46d6721
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2023-03-18 00:37:30 +0200

    Craete the backend

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Greeting.create(greeting: "Hello World!")
+Greeting.create(greeting: "Hello there!")
+Greeting.create(greeting: "hi")
+Greeting.create(greeting: "how are you doing?")
+Greeting.create(greeting: "whats up!")
